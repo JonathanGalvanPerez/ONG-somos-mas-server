0899ea5e9435ab76c43c145335fa27953c5df80a
Braulio Salazar <brauliot.salazar@hotmail.com>
2021-05-26 15:09:03 -0300

    Request http get / news, It only brings the type: news

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors')
 require('dotenv').config()
+require('./db');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const newsRouter = require('./routes/news');
 
 const app = express();
 app.use(cors())
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
