2f3405fdfdfb31cff6f1f245a369a974c282ad58
Fabo <heckiz@Heckiz.dev>
2021-05-30 00:30:19 -0300

    replace sendGrid by nodemailer as method to send emails

--- middlewares/sendMail.js
+++ middlewares/sendMail.js
@@ -1,25 +1,37 @@
-const sgMail = require('@sendgrid/mail');
+const { response } = require('express');
+const nodemailer = require('nodemailer');
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+ async function sendEmail(req, res, next) {
 
-module.exports = async function sendMail(req, res, next) {
+    const senderEmail = nodemailer.createTransport({
+        service: process.env.NODEMAILER_SENDER_SERVICE,
+        auth: {
+            user: process.env.NODEMAILER_SENDER_USER,
+            pass: process.env.NODEMAILER_SENDER_PASSWORD
+        }
+    });
 
-    const {senderEmail, recipientEmail, subject, message}= req.body;
+    const { emailTo, subject, message} = req.body;
 
-    const msg = {
-        to: senderEmail,
-        from: recipientEmail,
+    const mailOptions = {
+        from: process.env.NODEMAILER_SENDER_USER,
+        to: emailTo,
         subject: subject,
-        text: message,
+        text: message
     };
+    console.log(mailOptions)
+
 
     try {
-       const res = await sgMail.send(msg);
-       console.log(res);
-    } catch (error) {
-        console.error(error);
+        const info = await senderEmail.sendMail(mailOptions);
+        res.status(200).json({ ok: info.response });
+        next()
 
+    } catch (error) { 
+        console.error(error.message);
     }
 
-    next();
 }
+
+
+module.exports = sendEmail;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
+const sendMail = require('../middlewares/sendMail');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+// test route for send email
+router.post('/test', sendMail,(req, res)=>{
+    res.send('nodemailer')
+})
 module.exports = router;
