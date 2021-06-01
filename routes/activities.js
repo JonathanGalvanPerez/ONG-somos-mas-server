daef81dc3ee55cf1ea8033234ec8038e4227b79d
Braulio Salazar <brauliot.salazar@hotmail.com>
2021-06-01 12:06:30 -0300

    Code cleanup

--- routes/activities.js
+++ routes/activities.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
 const { activities, Sequelize } = require("../models");
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+const secretJwt = process.env.TOKEN_SECRET;
+
 
 router.post('/', async (req, res) =>{
     try {
         let name = req.body.name
         let content = req.body.content
 
-
+        
         if( !name || name.trim().length=== 0 || !content || content.trim().length===0) throw new Error('Falto enviar información')
 
         let post = await activities.create(req.body);
