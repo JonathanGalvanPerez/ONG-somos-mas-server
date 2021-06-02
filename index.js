3788af88510555b19308fe7cf01dedc9b95ddf23
Alejandro Sosa <agsosa74@gmail.com>
2021-06-01 21:35:04 -0300

    Fixes & reorganization

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const app = require('./app');
-const port = process.env.PORT ? process.env.PORT : 3000
+const port = process.env.PORT ? process.env.PORT : 3000;
+const { sequelize } = require("./models");
 
+const runServer = async () => {
+  try {
+    await sequelize.authenticate();
 
-app.listen(port, () => {
-    console.log("Running server at port: " + port);
+    console.log('DB Connection has been established successfully.');
 
-});
+    app.listen(port, () => {
+      console.log('Running server at port: ' + port);
+    });
+  } catch (error) {
+    console.error('DB Unable to connect to the database:', error);
+  }
+};
+
+runServer();
