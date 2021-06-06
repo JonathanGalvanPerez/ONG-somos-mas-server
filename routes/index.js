526d5a67574aa3e80921304e80d571158bff382b
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-06-06 18:18:19 -0300

    test contact email

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
-const sendEmail = require('../middlewares/sendEmail');
+const { contactEmail, sendEmail, getCredentials } = require('../middlewares/sendEmail');
 var router = express.Router();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 // test route for send email
-router.post('/test', sendEmail,(req, res)=>{
-    res.send('nodemailer')
-})
+router.post('/test', contactEmail, (req, res)=>{
+  sendEmail(req.body.email, 'Hola ' + req.body.name, req.html)
+    .then(() => res.send('termine de enviar el mail'))
+    .catch(error => {
+      console.log(error);
+      res.status(500).send({ error: error.message });
+    });
+});
+
 module.exports = router;
