50112ef34575e2560a3f2fce268ce40e45f90dc9
Mariano Collavino <mcollavino64@gmail.com>
2021-06-08 18:44:11 -0300

    add OrganizationContact to  endpoint response

--- routes/organizations.js
+++ routes/organizations.js
@@ -3,10 +3,14 @@ const path = require('path');
 const express = require("express");
 const router = express.Router();
 
+const {OrganizationContact, Sequelize} = require("../models/");
+
 const organizationdDataPath = path.join(__dirname, '../public/json/organization_data.json');
+
 
 router.get("/1/public", async (req, res) => {
     try {
+        const orgContact = await OrganizationContact.findAll(); //OT34-79
         const data = fs.readFileSync(organizationdDataPath, 'utf8');
         const parsedData = JSON.parse(data);
         res.status(200).json({
@@ -14,7 +18,8 @@ router.get("/1/public", async (req, res) => {
             image: parsedData.image,
             phone: parsedData.phone,
             address: parsedData.address,
-            welcomeText: parsedData.welcomeText
+            welcomeText: parsedData.welcomeText,
+            orgContact
         });
     } catch (e) {
         console.error(e.message);
