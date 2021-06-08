5a03575b01dcbe0fe1f3cdd1b295d9e72d007759
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-06-08 02:27:48 -0300

    remove email test route

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,10 @@
 var express = require('express');
-const { contactEmail, sendEmail, getCredentials } = require('../middlewares/sendEmail');
+const { contactEmail, sendEmail, getCredentials } = require('../services/sendEmail');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-// test route for send email
-router.post('/test', contactEmail, (req, res)=>{
-  getCredentials()
-    .then(() => {
-      return sendEmail(req.body.email, 'Hola ' + req.body.name, req.html);
-    })
-    .then(() => res.send('termine de enviar el mail'))
-    .catch(error => {
-      console.log(error);
-      res.status(500).send({ error: error.message });
-    });
 });
 
 module.exports = router;
