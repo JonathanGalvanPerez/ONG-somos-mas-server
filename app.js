d00ca610256619bd031cee260d85ec7357b1f2d7
Braulio Salazar <brauliot.salazar@hotmail.com>
2021-06-09 13:32:42 -0300

    The get / members endpoint is generated

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const contactsRouter = require("./routes/contacts");
 const organizationsRouter = require("./routes/organizations");
 const newsRouter = require("./routes/news.routes");
 const activitiesRouter =require ('./routes/activities');
+const membersRouter = require('./routes/members')
 
 const app = express();
 app.use(cors());
@@ -32,6 +33,7 @@ app.use("/organizations", organizationsRouter);
 app.use("/news", newsRouter);
 app.use("/contacts", contactsRouter);
 app.use("/activities", activitiesRouter);
+app.use("/members", membersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
