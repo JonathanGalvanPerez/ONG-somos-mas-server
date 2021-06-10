8d73f87da78cfeebae715da5824a64e4b9838100
Facundo Martinez <facundomartinezesc@gmail.com>
2021-06-10 11:22:14 -0300

    added to route members the route DELETE

--- routes/members.js
+++ routes/members.js
@@ -5,40 +5,32 @@ const { members, Sequelize } = require("../models");
 const authorize = require("../middlewares/authorize");
 const Role = require("../models/role.module");
 
+router.get("/", authorize(Role.Admin), async (req, res) => {
+  try {
+    let member = await members.findAll();
 
+    res.json(member);
+  } catch (e) {
+    console.error(e.message);
+    res.status(413).send({ Error: e.message });
+  }
+});
 
+router.delete("/:id", authorize(Role.Admin), async (req, res) => {
+  const { id } = req.params;
+  try {
+    const member = await members.findOne({ where: { id } });
 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-router.get('/', authorize(Role.Admin), async (req, res) =>{
-        try {
-
-            let member = await members.findAll();
-            
-            res.json(member)
-    
-        } catch (e) {
-            console.error(e.message);   
-            res.status(413).send({"Error": e.message});
-        }
-        
-        });
-
-
-
+    if (!member) {
+      res.json({ Error: "The member you want to delete does not exist" });
+    }
+    await members.destroy({
+      where: { id },
+    });
+    res.json({ Success: "Member deleted successfully" });
+  } catch (error) {
+    res.send({ Error: error.message });
+  }
+});
 
 module.exports = router;
