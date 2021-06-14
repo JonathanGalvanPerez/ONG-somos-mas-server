7bcc0ec9481fa6f83ff5edb7cdfc613c508efe18
alan-valenzuela93 <alan.caedus@outlook.com>
2021-06-14 13:11:27 -0300

    fixed error in put

--- controllers/testimonials.controller.js
+++ controllers/testimonials.controller.js
@@ -15,24 +15,25 @@ exports.createTestimony = async (req, res) => {
 };
 
 exports.updateTestimony = async (req, res) => {
+    const { id } = req.params
     const { name, content, image } = req.body;
-    try {
-        const testimony = await testimonials.findOne({ where: { id: req.params.id } });
-             
-    } catch (error) {
-        console.log(error)
-        res.status(404).json({
-            message: "Not found",
-            data: {}
-        })        
-    }
-    if(testimony.length > 0){
-        testimony.forEach(async element => {
-            await element.update({
+    
+    const allTestimonials = await testimonials.findAll({
+        attributes: ['name', 'content', 'image'],
+        where: {
+            id
+        }
+    });
+    if (allTestimonials.length > 0){
+        allTestimonials.forEach(async testimony => {
+            await testimony.update({
                 name,
                 content,
                 image
             })
         });
+    } else {
+        return res.status(404).json({message: "Not found"})
     }
+
 }
