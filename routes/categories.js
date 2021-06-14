8b0034955c8ee3b7bb46a62357f2d4fbee67b0dc
Samuel Benitez <samuelebenitez21@gmail.com>
2021-06-14 19:39:09 -0300

    added endpoint to create category

---
+++ routes/categories.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const { Category, Sequelize } = require("../models");
+
+const authorize = require("../middlewares/authorize");
+const Role = require("../models/role.module");
+
+router.post("/", authorize(Role.Admin), async (req, res) => {
+  try {
+    const { name, description } = req.body;
+
+    if (!name || name.length === 0)
+      res.status(413).json({ error: "Falta enviar información" });
+
+    if (typeof name !== "string")
+      res
+        .status(413)
+        .json({ error: "El nombre de la categoría debe ser un string" });
+
+    await Category.create({ name, description });
+    res.json({ success: "La categoría se ha creado correctamente" });
+  } catch (e) {
+    res.status(413).json({ error: e.message });
+  }
+});
+
+module.exports = router;
