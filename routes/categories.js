b5e78743623d97c1e5ba03149e4e327110e4bad0
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-06-19 13:40:29 -0300

    add res status code

--- routes/categories.js
+++ routes/categories.js
@@ -23,9 +23,7 @@ router.post("/", authorize(Role.Admin), async (req, res) => {
       res.status(400).json({ error: "Falta enviar información" });
 
     if (typeof name !== "string")
-      res
-        .status(400)
-        .json({ error: "El nombre de la categoría debe ser un string" });
+      res.status(400).json({ error: "El nombre de la categoría debe ser un string" });
 
     await Categories.create({ name, description });
     res.json({ success: "La categoría se ha creado correctamente" });
@@ -43,9 +41,7 @@ router.delete("/:id", authorize(Role.Admin), async (req, res) => {
     });
 
     if (!category || category.length === 0)
-      res
-        .status(400)
-        .json({ error: "La categoría que se quiere eliminar no existe" });
+      res.status(400).json({ error: "La categoría que se quiere eliminar no existe" });
 
     await Categories.destroy({
       where: { id: categoryId },
@@ -63,23 +59,27 @@ router.put("/:idCategory", authorize(Role.Admin) , async (req, res) =>{
       let id = req.params.idCategory
       
       //Validate
-      if( !name || name.trim().length=== 0) throw new Error('Falto enviar información')
+      if( !name || name.trim().length=== 0) {
+        res.status(400).send({ error: "El nombre de la categoria no puede estar vacio"})
+      }
 
       
       let category = await Categories.findAll({
-          where:{id: id}
+        where:{id: id}
       });
       console.log(category);
-      if(category.length === 0) throw new Error('La Categoria seleccionada no existe')
+      if(category.length === 0) {
+        res.status(404).send({ error: "No se ha encontrado la categoria que intenta modificar" })
+      }
 
       category = await Categories.update(req.body,{
-          where : {id: id}
+        where : {id: id}
       });
-      res.json({success:'Se ha modificado correctamente'})
+      res.json({ success: "Se ha modificado correctamente" })
 
   } catch (e) {
       console.error(e.message);   
-      res.status(500).send({"Error": e.message});
+      res.status(500).send({ error: e.message });
   }
 
 })
