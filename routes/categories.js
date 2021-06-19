cb748f652fb23010f815aca3717fa70467dae765
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-06-19 12:29:49 -0300

    correct res status code

--- routes/categories.js
+++ routes/categories.js
@@ -11,7 +11,7 @@ router.get("/", async (req, res) => {
     });
     res.json(categoryNames);
   } catch (e) {
-    res.status(413).json({ error: e.message });
+    res.status(500).json({ error: e.message });
   }
 });
 
@@ -20,17 +20,17 @@ router.post("/", authorize(Role.Admin), async (req, res) => {
     const { name, description } = req.body;
 
     if (!name || name.length === 0)
-      res.status(413).json({ error: "Falta enviar información" });
+      res.status(400).json({ error: "Falta enviar información" });
 
     if (typeof name !== "string")
       res
-        .status(413)
+        .status(400)
         .json({ error: "El nombre de la categoría debe ser un string" });
 
     await Categories.create({ name, description });
     res.json({ success: "La categoría se ha creado correctamente" });
   } catch (e) {
-    res.status(413).json({ error: e.message });
+    res.status(500).json({ error: e.message });
   }
 });
 
@@ -44,7 +44,7 @@ router.delete("/:id", authorize(Role.Admin), async (req, res) => {
 
     if (!category || category.length === 0)
       res
-        .status(413)
+        .status(400)
         .json({ error: "La categoría que se quiere eliminar no existe" });
 
     await Categories.destroy({
@@ -52,35 +52,34 @@ router.delete("/:id", authorize(Role.Admin), async (req, res) => {
     });
     res.json({ success: "La categoría se ha borrado correctamente" });
   } catch (e) {
-    res.status(413).send({ error: e.message });
+    res.status(500).send({ error: e.message });
   }
 });
 
 
-router.put("/:idCategory",authorize(Role.Admin) , async (req, res) =>{
+router.put("/:idCategory", authorize(Role.Admin) , async (req, res) =>{
   try {
       let name=req.body.name;
-      let description=req.body.description;
       let id = req.params.idCategory
       
       //Validate
       if( !name || name.trim().length=== 0) throw new Error('Falto enviar información')
 
       
-      let categority = await Categories.findAll({
+      let category = await Categories.findAll({
           where:{id: id}
       });
-      console.log(categority);
-      if(categority.length === 0) throw new Error('La Categoria seleccionada no existe')
+      console.log(category);
+      if(category.length === 0) throw new Error('La Categoria seleccionada no existe')
 
-      categority = await Categories.update(req.body,{
+      category = await Categories.update(req.body,{
           where : {id: id}
       });
       res.json({success:'Se ha modificado correctamente'})
 
   } catch (e) {
       console.error(e.message);   
-      res.status(413).send({"Error": e.message});
+      res.status(500).send({"Error": e.message});
   }
 
 })
