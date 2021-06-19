74e05d32b1599766cdbf3108fe8db59c1b165153
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-06-18 22:01:23 -0300

    fix categories model name

--- routes/categories.js
+++ routes/categories.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
-const { Category, Sequelize } = require("../models");
+const { Categories, Sequelize } = require("../models");
 
 const authorize = require("../middlewares/authorize");
 const Role = require("../models/role.module");
 
 router.get("/", async (req, res) => {
   try {
-    const categoryNames = await Category.findAll({
+    const categoryNames = await Categories.findAll({
       attributes: ["id", "name","description"],
     });
     res.json(categoryNames);
@@ -27,7 +27,7 @@ router.post("/", authorize(Role.Admin), async (req, res) => {
         .status(413)
         .json({ error: "El nombre de la categoría debe ser un string" });
 
-    await Category.create({ name, description });
+    await Categories.create({ name, description });
     res.json({ success: "La categoría se ha creado correctamente" });
   } catch (e) {
     res.status(413).json({ error: e.message });
@@ -38,7 +38,7 @@ router.delete("/:id", authorize(Role.Admin), async (req, res) => {
   try {
     let categoryId = req.params.id;
 
-    let category = await Category.findAll({
+    let category = await Categories.findAll({
       where: { id: categoryId },
     });
 
@@ -47,24 +47,24 @@ router.delete("/:id", authorize(Role.Admin), async (req, res) => {
         .status(413)
         .json({ error: "La categoría que se quiere eliminar no existe" });
 
-    await Category.destroy({
+    await Categories.destroy({
       where: { id: categoryId },
     });
-    res.json({ succes: "La categoría se ha borrado correctamente" });
+    res.json({ success: "La categoría se ha borrado correctamente" });
   } catch (e) {
     res.status(413).send({ error: e.message });
   }
 });
 
 
-router.put("/:idCategority",authorize(Role.Admin) , async (req, res) =>{
+router.put("/:idCategory",authorize(Role.Admin) , async (req, res) =>{
   try {
       let name=req.body.name;
       let description=req.body.description;
-      let id = req.params.idCategority
+      let id = req.params.idCategory
       
       //Validate
-      if( !name || name.trim().length=== 0 || !description || description.trim().length===0) throw new Error('Falto enviar información')
+      if( !name || name.trim().length=== 0) throw new Error('Falto enviar información')
 
       
       let categority = await Categories.findAll({
@@ -76,7 +76,7 @@ router.put("/:idCategority",authorize(Role.Admin) , async (req, res) =>{
       categority = await Categories.update(req.body,{
           where : {id: id}
       });
-      res.json({succes:'Se ha modificado correctamente'})
+      res.json({success:'Se ha modificado correctamente'})
 
   } catch (e) {
       console.error(e.message);   
