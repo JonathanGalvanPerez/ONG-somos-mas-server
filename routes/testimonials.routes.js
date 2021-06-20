1364aa97812abfe3b53545aaa2545bf36eeccd06
Fabo <heckiz@Heckiz.dev>
2021-06-20 20:38:21 -0300

    fix testimonials controller and start manual test

--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -24,15 +24,18 @@ router.post(
 
 router.put('/:id', authorize(Role.Admin), (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
     else next();
-   }
+},
+    controller.updateTestimony
 )
 
 router.delete('/:id', authorize(Role.Admin), (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()})
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
     else next();
-});
+},
+    controller.deleteTestimony
+);
 
 module.exports = router;
