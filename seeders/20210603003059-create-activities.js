a704293288f7ed20b14ded88a47d444dab74a33d
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-06-22 21:04:32 -0300

    add down migration function

--- seeders/20210603003059-create-activities.js
+++ seeders/20210603003059-create-activities.js
@@ -44,11 +44,8 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    
+    await queryInterface.bulkDelete('Activities', null, {});
+    
   },
 };
