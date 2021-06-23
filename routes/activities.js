2959a289ae17ec172f29643fa82c3b00799d1eff
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-06-22 22:24:40 -0300

    fix errors. correct status code. remove unnecessary querys

--- routes/activities.js
+++ routes/activities.js
@@ -22,7 +22,6 @@ router.get('/', async (req, res) => {
         const result = await Activity.findAll();
         res.status(200).send(result);
     } catch (error) {
-        console.log(error);
         console.error(error.message);
         res.status(500).send({ error: error.message });
     }
@@ -30,29 +29,18 @@ router.get('/', async (req, res) => {
 
 router.put('/:id', authorize(Role.Admin) , async (req, res) =>{
     try {
-        let name=req.body.name;
-        let image=req.body.image;
-        let content=req.body.content;
         let id = req.params.id;
 
-        if( !name || name.trim().length=== 0 || !image || image.trim().length===0|| !content || content.trim().length===0)
-            res.status(400).send({ error: 'Falto enviar información' });
-
-        let activity = await Activity.findAll({
-            where:{id: id}
-        });
-
-        if(activity.length === 0)
-            res.status(404).send({ error: 'La Actividad ingresada no existe' });
-
-        activity = await Activity.update(req.body,{
+        let result = await Activity.update(req.body,{
             where : {id: id}
         });
-        res.json({ success:'Se ha modificado correctamente' })
-
+        // Update retorna una lista donde el primer elemento es el numero de filas afectadas
+        if(result[0]==0)
+            res.status(404).send({ error: 'La Actividad ingresada no existe' });
+        else
+            res.status(200).send({ success:'Se ha modificado correctamente' })
 
     } catch (error) {
-        console.log(error);
         console.error(e.message);   
         res.status(500).send({ error: e.message});
     }
@@ -65,14 +53,12 @@ router.post('/', authorize(Role.Admin), async (req, res) =>{
         let name = req.body.name
         let content = req.body.content
 
-        
-
         if( !name || name.trim().length=== 0 || !content || content.trim().length===0)
-            res.status(400).send('Falto enviar información');
-
-        let post = await Activity.create(req.body);
-        
-        res.json(post)
+            res.status(400).end('Falto enviar información');
+        else {
+            let post = await Activity.create(req.body);
+            res.status(201).json(post)
+        }
 
     } catch (e) {
         console.error(e.message);   
@@ -83,19 +69,16 @@ router.post('/', authorize(Role.Admin), async (req, res) =>{
 router.delete("/:id", authorize(Role.Admin), async (req, res) => {
     try {
         let activityId = req.params.id;
-    
-        let activity = await Activity.findAll({
-        where: { id: activityId },
+        let result = await Activity.destroy({
+            where: { id: activityId },
         });
-    
-        if (!activity || activity.length === 0)
-        res.status(400).send({ error: "La actividad que se quiere eliminar no existe" });
-    
-        await Activity.destroy({
-        where: { id: activityId },
-        });
-        res.json({ success: "La actividad se ha borrado correctamente" });
+        // Destroy retorna el numero de filas eliminadas
+        if(result === 0)
+            res.status(400).send({ error: "La actividad que se quiere eliminar no existe" });
+        else
+            res.json({ success: "La actividad se ha borrado correctamente" });
     } catch (e) {
+        console.error(e.message);   
         res.status(500).send({ error: e.message });
     }
 }); 
