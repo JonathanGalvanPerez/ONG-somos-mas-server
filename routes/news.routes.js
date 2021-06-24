0cb002bcccd10ceddcb26b7e82a05f5e983dc987
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-06-24 06:19:53 -0300

    add role authorization to POST, PUT and DELETE methods on /news

--- routes/news.routes.js
+++ routes/news.routes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
-const {getAllNews, createNew, getNew, deleteNew, updateNew} = require('../controllers/news.controller')
+const {getAllNews, createNew, getNew, deleteNew, updateNew} = require('../controllers/news.controller');
+const authorize = require("../middlewares/authorize");
+const Role = require("../models/role.module");
 
 router.get("/", getAllNews);
-router.post("/", createNew);
+router.post("/", authorize(Role.Admin), createNew);
 
 router.get("/:id", getNew);
-router.delete("/:id", deleteNew);
-router.put('/:id', updateNew);
+router.delete("/:id", authorize(Role.Admin), deleteNew);
+router.put('/:id', authorize(Role.Admin), updateNew);
 
 module.exports = router;
